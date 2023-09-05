47b5d1be981f12975f34610e382337fcb61b0e00
AZHARRRRRRRRR <143793259+AZHARRRRRRRRR@users.noreply.github.com>
2023-09-05 11:15:51 +0800

    Add files via upload

---
+++ callingmodule.py
@@ -0,0 +1,7 @@
+import mymodule
+
+mymodule.student1("Rafi", "18DDT21F2016")
+
+mymodule.student2("Asyraf", "18DDT21F2001")
+
+mymodule.student3("Azhar", "18DDT21F2019")
---
+++ mymodule.py
@@ -0,0 +1,13 @@
+def student1(name, nomborMatriks):
+    print("nama: " + name)
+    print("nombor matriks: " + nomborMatriks)
+    print()
+
+def student2(name, nomborMatriks):
+    print("nama: " + name )
+    print("nombor matriks: " + nomborMatriks)
+    print()
+
+def student3(name, nomborMatriks):
+    print("nama: " + name )
+    print("nombor matriks: " + nomborMatriks )
