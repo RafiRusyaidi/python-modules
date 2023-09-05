a188358fe8ecf7b1c75a79e45b76d0b4f224d1e4
AZHARRRRRRRRR <143793259+AZHARRRRRRRRR@users.noreply.github.com>
2023-09-05 12:24:57 +0800

    tambah baru

---
+++ main.py
@@ -0,0 +1,14 @@
+import exercisemodule
+import dataPelajar
+import maklumatPelajar
+
+
+def main():
+    exercisemodule.student("Azhar", "18DDT21F2019")
+    dataPelajar.info1("Asyraf")
+    dataPelajar.info2("18DDT21F2001")
+    maklumatPelajar.maklumat("Rafi", "18DDT21F2016")
+
+
+if __name__ == "__main__" :
+    main()
