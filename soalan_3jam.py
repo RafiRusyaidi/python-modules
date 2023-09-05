9b278f2a1dcb9cb4655d3aae877fb3d9f25c2140
RafiRusyaidi <143974288+RafiRusyaidi@users.noreply.github.com>
2023-09-05 11:17:44 +0800

    Delete soalan_3jam.py

--- soalan_3jam.py
+++
@@ -1,9 +0,0 @@
-def main():
-    name = input("please enter you name: ")
-    matrik = input("please enter you matrik number: ")
-    print(name.upper())
-    print(matrik.upper())
-    print("program =", matrik[2:5].upper())
-
-
-main()
