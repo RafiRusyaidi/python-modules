612999434bc1858d3f95cfd871bf2d25cfe1ffd5
RafiRusyaidi <143974288+RafiRusyaidi@users.noreply.github.com>
2023-09-05 10:47:17 +0800

    Add files via upload

---
+++ soalan_3jam.py
@@ -0,0 +1,9 @@
+def main():
+    name = input("please enter you name: ")
+    matrik = input("please enter you matrik number: ")
+    print(name.upper())
+    print(matrik.upper())
+    print("program =", matrik[2:5].upper())
+
+
+main()
